137a36df840d7c16599f997b0095192550032503
zhenkungg <2383731880@qq.com>
2017-01-11 16:13:17 +0800

    Initial Commit

---
+++ Zhima/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Zhima
+//
+//  Created by 古振坤 on 2017/1/11.
+//  Copyright © 2017年 zkgg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Zhima/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zhima
+//
+//  Created by 古振坤 on 2017/1/11.
+//  Copyright © 2017年 zkgg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
