a8e178992be0fea7e3e628e666ccb848f277e423
Json2017 <json@JsondeMac-mini.local>
2017-01-12 17:50:52 +0800

    个人中心
    1

--- Zhima/PersonViewController.m
+++ Zhima/PersonViewController.m
@@ -20,12 +20,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     _tableView = [[UITableView alloc]init];
-    _tableView.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y+64, CGRectGetMaxX(self.view.frame), CGRectGetMaxY(self.view.frame));
+    //_tableView.frame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y+64, CGRectGetMaxX(self.view.frame), CGRectGetMaxY(self.view.frame));
+    _tableView.frame = self.view.frame;
     _tableView.delegate = self;
     _tableView.dataSource = self;
+    _tableView.tableHeaderView = [[[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner:nil options:nil]lastObject];
+    _tableView.tableHeaderView.frame = CGRectMake(CGRectGetMinX(_tableView.frame), CGRectGetMinY(_tableView.frame), CGRectGetMaxX(_tableView.frame), 200);
     [self.view addSubview:_tableView];
+    _tableView.tableFooterView = [[UIView alloc]initWithFrame:CGRectZero];
+    _tableView.backgroundColor = [UIColor groupTableViewBackgroundColor];
     
-    _titles = @[@"个人资料",@"课程设置",@"我的好友",@"我的订单",@"我的收入"];
+    
+    
+    _titles = @[@"个人资料",@"课程设置",@"我的好友",@"我的订单",@"我的收入",@"设置"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,27 +40,50 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = YES;
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    self.navigationController.navigationBarHidden = NO;
+}
+
 #pragma mark - UITableViewDelegate
 
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
-    return 2;
+    return 3;
 }
     
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     if (section == 1) {
-        return 1;
-    }else
-    return 4;
+        return 4;
+    }
+    return 1;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [[UITableViewCell alloc]init];
-    if (indexPath.section == 1) {
+    if (indexPath.section == 0) {
         cell.textLabel.text = _titles[0];
+    }else if(indexPath.section == 2){
+        cell.textLabel.text = _titles[5];
     }else
     cell.textLabel.text = _titles[indexPath.row];
+    
+    cell.imageView.image = [UIImage imageNamed:@"doc"];
+    
     return cell;
 }
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 10;
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
+    return 1;
+}
+
 /*
 #pragma mark - Navigation
 
