e9cfb725d89e6b479cd4ee2d702372a08f2305a1
zhenkungg <2383731880@qq.com>
2017-02-13 17:13:16 +0800

    zk
    添加afn，svp，注册接口对接

---
+++ Podfile
@@ -0,0 +1,23 @@
+project 'Zhima.xcodeproj'
+
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Zhima' do
+  # Uncomment this line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+pod 'AFNetworking', '~> 3.1'
+pod 'SVProgressHUD', '~> 2.1'
+  # Pods for Zhima
+
+  target 'ZhimaTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ZhimaUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- Zhima/NewsViewController.m
+++ Zhima/NewsViewController.m
@@ -13,7 +13,8 @@
 #import "AppDelegate.h"
 #import "ChatDemoHelper.h"
 #import "PersonalViewController.h"
-
+#import <AFNetworking.h>
+#import <SVProgressHUD.h>
 @interface NewsViewController ()<UITableViewDataSource,UITableViewDelegate>
 //@property (weak, nonatomic) IBOutlet UITableView *newstableV;
 
@@ -40,39 +41,116 @@
   }
 
 - (IBAction)registeredClick:(id)sender {
-    if(self.accountTF.text && self.accountTF.text){
-        EMError *error = [[EMClient sharedClient] registerWithUsername:self.accountTF.text password:self.passwordTF.text];
+    NSString *url = @"http://118.89.45.205/users/reg";
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    [manager.requestSerializer setValue:@"ios" forHTTPHeaderField:@"User-Agent"];
+    NSDictionary *parameters = @{
+                                 @"username":self.accountTF.text,
+                                 @"password":self.passwordTF.text,
+                                 @"usertype":@"1"
+                                 };
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:parameters options:NSJSONWritingPrettyPrinted error:&error];
+    NSString *jsonString = [[NSString alloc ]initWithData:jsonData encoding:NSUTF8StringEncoding];
+    NSLog(@"%@",jsonString);
+    
+    
+    [manager POST:url parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
         
-        if (error==nil) {
-            NSLog(@"注册成功");
-        }else{
-            NSLog(@"注册失败 :%@",error);
-           
-        }
-    }
+        NSLog(@"%@",formData);
+        
+    } progress:^(NSProgress * _Nonnull uploadProgress) {
+        //打印下上传进度
+        NSLog(@"%lf",1.0 *uploadProgress.completedUnitCount / uploadProgress.totalUnitCount);
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"请求成功：%@",responseObject);
+        NSString *str = [[NSString alloc]initWithData:responseObject encoding:NSUTF8StringEncoding];
+        NSLog(@"%@",str);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"请求失败:%@",error);
+    }];
+
+    
+//    if(self.accountTF.text && self.accountTF.text){
+//        EMError *error = [[EMClient sharedClient] registerWithUsername:self.accountTF.text password:self.passwordTF.text];
+//        
+//        if (error==nil) {
+//            NSLog(@"注册成功");
+//        }else{
+//            NSLog(@"注册失败 :%@",error);
+//           
+//        }
+//    }
     
 }
 
 
 - (IBAction)loginClick:(id)sender {
-    if(self.accountTF.text && self.accountTF.text){
-        EMError *error = [[EMClient sharedClient] loginWithUsername:self.accountTF.text password:self.passwordTF.text];
+    NSString *url = @"http://118.89.45.205/users/login";
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    [manager.requestSerializer setValue:@"ios" forHTTPHeaderField:@"User-Agent"];
+    NSDictionary *parameters = @{
+                                 @"username":self.accountTF.text,
+                                 @"password":self.passwordTF.text,
+                                 @"usertype":@"1"
+                                 };
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:parameters options:NSJSONWritingPrettyPrinted error:&error];
+    NSString *jsonString = [[NSString alloc ]initWithData:jsonData encoding:NSUTF8StringEncoding];
+    NSLog(@"%@",jsonString);
+    
+    
+    [manager POST:url parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
         
-        if (error==nil) {
-            NSLog(@"登录成功");
-            //切换聊天列表为跟控制器
-            NewsConversationListViewController  *listVC = [[NewsConversationListViewController alloc] init];
-            listVC.title = @"消息列表";
-            
-            [ChatDemoHelper shareHelper].mainVC = listVC;
-            
-            //[AppDelegate getAppDelegate].window.rootViewController = [[UINavigationController alloc] initWithRootViewController:listVC];
-            [self.navigationController pushViewController:listVC animated:YES];
-            
-        }else{
-            NSLog(@"登录失败 :%@",error);
-        }
-    }
+        NSLog(@"%@",formData);
+        //        NSString *str = [[NSString alloc]initWithData:formData encoding:NSUTF8StringEncoding];
+        //        NSLog(@"%@",str);
+    } progress:^(NSProgress * _Nonnull uploadProgress) {
+        //打印下上传进度
+        NSLog(@"%lf",1.0 *uploadProgress.completedUnitCount / uploadProgress.totalUnitCount);
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"请求成功：%@",responseObject);
+        NSString *str = [[NSString alloc]initWithData:responseObject encoding:NSUTF8StringEncoding];
+        NSLog(@"%@",str);
+        
+        NewsConversationListViewController  *listVC = [[NewsConversationListViewController alloc] init];
+        listVC.title = @"消息列表";
+        
+        [ChatDemoHelper shareHelper].mainVC = listVC;
+        
+        //[AppDelegate getAppDelegate].window.rootViewController = [[UINavigationController alloc] initWithRootViewController:listVC];
+        [self.navigationController pushViewController:listVC animated:YES];
+                
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"请求失败:%@",error);
+    }];
+
+//    if(self.accountTF.text && self.accountTF.text){
+//        EMError *error = [[EMClient sharedClient] loginWithUsername:self.accountTF.text password:self.passwordTF.text];
+//        
+//        if (error==nil) {
+//            NSLog(@"登录成功");
+//            //切换聊天列表为跟控制器
+//            NewsConversationListViewController  *listVC = [[NewsConversationListViewController alloc] init];
+//            listVC.title = @"消息列表";
+//            
+//            [ChatDemoHelper shareHelper].mainVC = listVC;
+//            
+//            //[AppDelegate getAppDelegate].window.rootViewController = [[UINavigationController alloc] initWithRootViewController:listVC];
+//            [self.navigationController pushViewController:listVC animated:YES];
+//            
+//        }else{
+//            NSLog(@"登录失败 :%@",error);
+//        }
+//    }
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
