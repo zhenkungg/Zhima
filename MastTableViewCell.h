e833a8a9ea5f5268b911d64ff7eec3fcff447989
zhenkungg <2383731880@qq.com>
2017-02-27 16:25:50 +0800

    zk
    学习报告

---
+++ MastTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MastTableViewCell.h
+//  Zhima
+//
+//  Created by 古振坤 on 2017/2/27.
+//  Copyright © 2017年 zkgg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MastTableViewCell : UITableViewCell
+
+@end
---
+++ Zhima/WentiTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WentiTableViewCell.h
+//  Zhima
+//
+//  Created by 古振坤 on 2017/2/27.
+//  Copyright © 2017年 zkgg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WentiTableViewCell : UITableViewCell
+
+@end
